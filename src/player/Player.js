24310a9c0aaaa7cff15e5d92280cf39211862889
Teddy Chen <teddychen1251@gmail.com>
2025-08-19 00:42:17 -0700

    Player body attempt 0

--- src/player/Player.js
+++ src/player/Player.js
@@ -1,10 +1,31 @@
 class Player {
+    #headCamera;
+    #bodyMesh;
     #hands = [];
-    #body;
     constructor(scene, xr) {
+        this.#headCamera = xr.baseExperience.camera;
+        this.#bodyMesh = BABYLON.MeshBuilder.CreateBox(
+            "playerBody", 
+            {
+                height: 0.6,
+                width: .35,
+                depth: .1,
+            }, 
+            scene
+        ); 
+        const bodyClone = this.#bodyMesh.clone();
+        scene.onBeforeRenderObservable.add(() => {
+            this.#bodyMesh.rotation.y = this.#headCamera.rotationQuaternion.toEulerAngles().y;
+            this.#bodyMesh.position = this.#headCamera.position.subtract(new BABYLON.Vector3(0, .45, 0.075));
+            bodyClone.position = this.#bodyMesh.position.add(new BABYLON.Vector3(0, 0, 0.5))
+        })
         xr.input.onControllerAddedObservable.add(controllerInputSource => {
             this.#hands.push(new Hand(controllerInputSource, scene));
         });
 
+    }
+
+    get headPosition() {
+        return this.#headCamera.position
     }
 }
